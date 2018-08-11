c7d70100c1aabe937ebecb0c0ec816961fe53fa7
SamYangBio <40067436+SamYangBio@users.noreply.github.com>
2018-08-12 00:48:28 +0800

    Update Basic_grammar.c

--- Basic_grammar.c
+++ Basic_grammar.c
@@ -6,10 +6,24 @@ so the C language has many similarities to the perl language.*/
 int main()
 {
 	int a,b,t;
-	scanf("%d%d", &a,&b); /*scanf function like perl-ARGV*/
+	scanf("%d%d", &a,&b); //scanf function like perl-ARGV
 	t = a ;
 	a = b ;
 	b = t;
 	printf("%d %d\n", a, b);
+	return 0;
+}
+
+/*---------------------------------three-number sort-----------------------------*/
+
+#include <stdio.h>
+int main()
+{
+	int a, b , c, t;
+	scanf("%d%d%d", &a, &b, &c);
+	if (a > b) { t = a; a = b; b = t;} //执行完毕之后a<=b
+	if (a > c) { t = a; a = c; c = t;} //执行完毕之后a<=c,且a<=b依然成立
+	if (b > c) { t = b; b = c; c = t;}
+	printf("%d %d %d\n", a, b, c);
 	return 0;
 }
